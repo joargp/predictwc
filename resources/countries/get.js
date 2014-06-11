1790b586aa7314becc3e60a0da32b2851ca22cec
Joar Gullestad Pettersen <joargp@gmail.com>
2014-06-11 23:19:18 +0200

    bla bla

--- resources/countries/get.js
+++ resources/countries/get.js
@@ -3,6 +3,6 @@
 //});
 // caused cyclic reference loop with countries already being fetched in groups
 
-dpd.groups.get({id: this.groupId }, function(result) {
-    this.group = result.name;
-});
+//dpd.groups.get({id: this.groupId }, function(result) {
+//    this.group = result.name;
+//});
